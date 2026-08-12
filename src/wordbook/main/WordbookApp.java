99375fc17bb67cbdc914b6eb506c21ca4125463f
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-08-12 13:46:23 +0900

    feat: ウィンドウ内のレイアウトを追加

--- src/wordbook/main/WordbookApp.java
+++ src/wordbook/main/WordbookApp.java
@@ -1,9 +1,14 @@
 package wordbook.main;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
-import wordbook.file.WordbookFileManager;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 public class WordbookApp {
 
@@ -15,12 +20,28 @@ public class WordbookApp {
 			frame.setSize(400, 400);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setLocation(100 , 100);
+			frame.setLayout(new BorderLayout());
 			
 			// 画面内部のパーツを配置
-			WordbookFileManager wfm = new WordbookFileManager();
-			wfm.loadFiles();
-			wfm.saveFiles();
-			wfm.loadFiles();
+			// 1.見出し（上部）
+			JLabel titleLabel = new JLabel("見出し", SwingConstants.CENTER);
+			frame.add(titleLabel, BorderLayout.NORTH);
+			
+			// 2.解説等（中央）
+			JLabel textArea = new JLabel("解説", SwingConstants.CENTER);
+			frame.add(textArea, BorderLayout.CENTER);
+			
+			// 3.ページ切り替え（下部）
+			JPanel bottomPanel = new JPanel();
+			bottomPanel.setLayout(new FlowLayout());
+			
+			JButton prevBtn = new JButton("前へ");
+			JButton nextBtn = new JButton("次へ");
+			
+			bottomPanel.add(prevBtn);
+			bottomPanel.add(nextBtn);
+			
+			frame.add(bottomPanel, BorderLayout.SOUTH);
 			
 			// 画面を表示
 			frame.setVisible(true);
