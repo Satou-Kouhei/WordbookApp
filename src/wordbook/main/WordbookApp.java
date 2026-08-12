25773efac477de83860b46cf80481d29d575c284
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-08-12 09:50:58 +0900

    feat: アプリウィンドウを表示できるようにする

--- src/wordbook/main/WordbookApp.java
+++ src/wordbook/main/WordbookApp.java
@@ -1,9 +1,24 @@
 package wordbook.main;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class WordbookApp {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO アプリ用のウィンドウを表示させる
+		SwingUtilities.invokeLater(() -> {
+			// 表示する画面の設定
+			JFrame frame = new JFrame("Wordbook App");
+			frame.setSize(400, 400);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocation(100 , 100);
+			
+			// 画面内部のパーツを配置
+			
+			// 画面を表示
+			frame.setVisible(true);
+		});
 
 	}
 
