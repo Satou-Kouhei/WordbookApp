9b901d6b9c598ae885517b6b8f66ad664f5ab2c1
Satou-Kouhei <kou.3145.h2@gmail.com>
2026-08-14 12:38:50 +0900

    feat: JSONを読み込みウィンドウに内容を表示する機能を作成
    - ウィンドウにリストの内容を反映するupdateDisplayメソッドを作成
    - ページ移動機能を作成

--- src/wordbook/main/WordbookApp.java
+++ src/wordbook/main/WordbookApp.java
@@ -2,18 +2,26 @@ package wordbook.main;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.io.IOException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
+import wordbook.file.Word;
+import wordbook.file.WordbookFileManager;
+
 public class WordbookApp {
+	private static int currentIdx;			// ファイルから何行目を読み込むか
+	private static List<Word> wordList;		// 読み込むデータのリスト
 
 	public static void main(String[] args) {
-		// TODO アプリ用のウィンドウを表示させる
+		// アプリ用のウィンドウを表示させる
 		SwingUtilities.invokeLater(() -> {
 			// 表示する画面の設定
 			JFrame frame = new JFrame("Wordbook App");
@@ -28,10 +36,12 @@ public class WordbookApp {
 			frame.add(titleLabel, BorderLayout.NORTH);
 			
 			// 2.解説等（中央）
-			JLabel textArea = new JLabel("解説", SwingConstants.CENTER);
+			JTextArea textArea = new JTextArea();
+			textArea.setLineWrap(true);
+			textArea.setEditable(false);
 			frame.add(textArea, BorderLayout.CENTER);
 			
-			// 3.ページ切り替え（下部）
+			// 3.ページ切り替えボタン（下部）
 			JPanel bottomPanel = new JPanel();
 			bottomPanel.setLayout(new FlowLayout());
 			
@@ -43,10 +53,56 @@ public class WordbookApp {
 			
 			frame.add(bottomPanel, BorderLayout.SOUTH);
 			
+			// JSONファイルを読み込んで、ウィンドウに見出しと解説をセット
+			WordbookFileManager fileManager = new WordbookFileManager();
+			try {
+				// ファイル読み込み
+				wordList = fileManager.loadJson();
+				
+				// 表示する見出しと解説をセットする
+				updateDisplay(titleLabel, textArea);
+				
+			} catch (IOException e) {
+				titleLabel.setText("エラー");
+				textArea.setText("JSONファイルの読み取り失敗");
+				e.printStackTrace();
+			}
+			
+			// ページ移動の動作
+			// 1.前へ
+			prevBtn.addActionListener(e -> {
+				if(wordList != null && currentIdx > 0) {
+					currentIdx--;
+					updateDisplay(titleLabel, textArea);
+				}
+			});
+			
+			// 2.次へ
+			nextBtn.addActionListener(e -> {
+				if(wordList != null && currentIdx < wordList.size() - 1) {
+					currentIdx++;
+					updateDisplay(titleLabel, textArea);
+				}
+			});
+			
 			// 画面を表示
 			frame.setVisible(true);
 		});
+	}
+	
+	/**
+	 * currentIdxから見出しと解説をセットするメソッド
+	 * @param label
+	 * @param area
+	 */
+	private static void updateDisplay(JLabel label, JTextArea area) {
+		// wordListがなければ早期リターンする
+		if(wordList == null || wordList.isEmpty()) return;
 		
+		// 見出しと解説をセットする
+		Word currentWord = wordList.get(currentIdx);
+		label.setText(currentWord.getWord());
+		area.setText(currentWord.getDescription());
 	}
 
 }
